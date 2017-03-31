fa8c9fca3559f4e0b9be6bd88598ba999c0524d1
Lou <lfranklin@deloitte.co.uk>
2017-03-31 15:40:51 +0100

    Initial setup based on Denny and prior projects

---
+++ data_helpers.py
@@ -0,0 +1,62 @@
+import numpy as np
+import re
+import itertools
+from collections import Counter
+
+
+def clean_str(string):
+    """
+    Tokenization/string cleaning for all datasets except for SST.
+    Original taken from https://github.com/yoonkim/CNN_sentence/blob/master/process_data.py
+    """
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string.strip().lower()
+
+def label_arr(y_text, labels):
+    y = []
+    for i in y_text:
+        y.append(get_vector_label(i,labels))
+    return np.asarray(y)
+
+def batch_iter(data, batch_size, num_epochs, shuffle=True):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int(len(data)/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            shuffled_data = data[shuffle_indices]
+        else:
+            shuffled_data = data
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index:end_index]
+
+def label_text(num_array, labels):
+    result = []
+    for n in num_array:
+        result.append(labels[int(n)])
+    return result
+
+def get_vector_label(tag, tag_list):
+    label = np.zeros(len(tag_list))
+    tag_index = tag_list.index(tag)
+    label[int(tag_index)] = 1
+    return label
